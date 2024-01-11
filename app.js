10fe3fc61cfee729da64d4a28ac0bfcb180b9c2f
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-01-11 13:50:50 +0530

    Login/Signup Backend Completed

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+var users = [];
+var user;
 
 const db = new pg.Client({
   user: "postgres",
@@ -19,9 +21,67 @@ const db = new pg.Client({
 });
 db.connect();
 
+// Function to check existence of user
+async function chkUser(email) {
+  const result = await db.query("SELECT email FROM users WHERE email = $1;", [
+    email,
+  ]);
+  if (result.rows.length == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
 // Login Page Route
 app.get("/", (req, res) => {
   res.render(__dirname + "/view/login.ejs");
+});
+
+// Signup Page Route
+app.post("/signup", async (req, res) => {
+  const status = await chkUser(req.body.email);
+  if (status) {
+    res.render(__dirname + "/view/login.ejs", {
+      error: "Entered Email is already registered, Please Login",
+    });
+  } else {
+    try {
+      await db.query(
+        "INSERT INTO users (name, email, password, sec_ques, sec_ans) VALUES ($1,$2,$3,$4,$5)",
+        [
+          req.body.name,
+          req.body.email,
+          req.body.password,
+          req.body.sec_ques,
+          req.body.sec_ans,
+        ]
+      );
+      res.render(__dirname + "/view/login.ejs");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+// Login Page Route
+app.post("/login", async (req, res) => {
+  try {
+    const result = await db.query(
+      "SELECT password FROM users WHERE email = ($1);",
+      [req.body.email]
+    );
+    let password = result.rows[0].password;
+    if (password === req.body.password) {
+      res.render(__dirname + "/view/index.ejs");
+    } else {
+      res.render(__dirname + "/view/login.ejs", {
+        error: "Entered Password or Email is Incorrect",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // Forgot Page Route
